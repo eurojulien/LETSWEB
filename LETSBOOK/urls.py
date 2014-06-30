c06706d17c7290040646a28330872d736b3f80a5
Julien Drolet-Noel <eurojulien@gmail.com>
2014-06-30 14:53:02 -0400

    Ajout des services pour etablissement, departements, cours, livres et comptes usagers

--- LETSBOOK/urls.py
+++ LETSBOOK/urls.py
@@ -4,6 +4,10 @@ from LETSBOOK import views
 
 urlpatterns = patterns('',
 
-    url(r'find_book', views.findBook, name='findBook'),
+    url('school',       views.addModifySchool,      name='addModifySchool'),
+    url('department',   views.addModifyDepartment,  name='addModifyDepartment'),
+    url('course',       views.addModifyCourse,      name='addModifyCourse'),
+    url(r'book',        views.addModififyBook,      name='addModifyBook'),
+    url(r'findBook',    views.findBook,             name='findBook'),
 
 )
