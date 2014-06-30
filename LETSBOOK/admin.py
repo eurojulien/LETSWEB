e0c1ee3cf39832c0c515e630a8b01e9ba89941f9
Julien Drolet-Noel <eurojulien@gmail.com>
2014-06-30 14:51:49 -0400

    Permet de gerer toutes les tables dans la section ADMIN

--- LETSBOOK/admin.py
+++ LETSBOOK/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from LETSBOOK.models import Livre
+from LETSBOOK.models import *
 
-admin.site.register(Livre)
+admin.site.register(Establishment)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Book)
+admin.site.register(Account)
