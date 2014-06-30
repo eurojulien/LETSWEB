8103da2e0912a66d831ca425ba2755c58b194648
Julien Drolet-Noel <eurojulien@gmail.com>
2014-06-30 14:53:23 -0400

    Code fonctionnel pour une ecole

--- LETSBOOK/views.py
+++ LETSBOOK/views.py
@@ -1,107 +1,193 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 from django.shortcuts import render
-from models import Book
+from models import *
 from django.http import HttpResponse, StreamingHttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from django import http
 from django.shortcuts import render, get_list_or_404
+
 import json
 
 # Description d'un livre
-_USER           = "student"     # Identificateur de l'utilisateur possedant le livre
-_TITLE          = "title"       # Titre du livre
-_AUTHOR         = "author"      # Auteur du livre
-_EDITION        = "edition"     # Edition du livre
-_SIGLE          = "sigle"       # Sigle du cours dans lequel le livre est demande
-_STATE          = "state"       # Etat du livre
-_DESCRIPTION    = "desc"        # Description du livre
-_PRICE          = "price"       # Prix du livre
-_ISBN           = "ISBN"        # Code a barre du livre
-_INTENT         = "intent"      # Intention de transaction du vendeur du livre
-_PICTURE        = "image"       # Image du livre
+_BOOK_USER           = "student"     # Identificateur de l'utilisateur possedant le livre
+_BOOK_TITLE          = "title"       # Titre du livre
+_BOOK_AUTHOR         = "author"      # Auteur du livre
+_BOOK_EDITION        = "edition"     # Edition du livre
+_BOOK_SIGLE          = "sigle"       # Sigle du cours dans lequel le livre est demande
+_BOOK_STATE          = "state"       # Etat du livre
+_BOOK_DESC           = "desc"        # Description du livre
+_BOOK_PRICE          = "price"       # Prix du livre
+_BOOK_ISBN           = "ISBN"        # Code a barre du livre
+_BOOK_INTENT         = "intent"      # Intention de transaction du vendeur du livre
+_BOOK_PICTURE        = "image"       # Image du livre
+
+# Description d'un etablissement scolaire
+_SCHOOL_NAME    = "name"
+_SCHOOL_STREET  = "street"
+_SCHOOL_CITY    = "city"
+_SCHOOL_ZIP     = "zipcode"
+_SCHOOL_TYPE    = "type"
+_SCHOOL_WEB     = "website"
+
+# Description d'un departement scolaire
+_DEPT_NAME          = "name"
+_DEPT_DESC          = "desc"
+_DEPT_SCHOOL_NAME   = "schoolName"
+
+# Description d'un cours scolaire
+_COURSE_SIGLE         = "sigle"
+_COURSE_NAME          = "name"
+_COURSE_DESC          = "desc"
+
+
+# Code de serveur
+# http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+_ADD_OR_MODIFY_RECORD_SUCCESS   = 201
+_ADD_OR_MODIFY_RECORD_FAIL      = 202
+
 
 # Create your views here.
-def findBook(request):
+def addModifySchool(request):
 
-    response = {}
+    school = Establishment()
 
-    if _ID in request.GET:
-        response[_ID] = str(request.GET[_ID])
+    if _SCHOOL_NAME in request.GET:
 
-    if _TITLE in request.GET:
-        response[_TITLE] = str(request.GET[_TITLE])
+        try:
+            school = Establishment.objects.get(name=request.GET[_SCHOOL_NAME])
 
-    if _AUTHOR in request.GET:
-        response[_AUTHOR] = str(request.GET[_AUTHOR])
+        except ObjectDoesNotExist:
+            school = Establishment(name=request.GET[_SCHOOL_NAME])
 
-    if _EDITION in request.GET:
-        response[_EDITION] = str(request.GET[_EDITION])
+    else:
+        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
 
-    if _SIGLE in request.GET:
-        response[_SIGLE] = str(request.GET[_SIGLE])
+    if _SCHOOL_STREET in request.GET:
+        school.street   = request.GET[_SCHOOL_STREET]
 
-    if _STATE in request.GET:
-        response[_STATE] = str(request.GET[_STATE])
+    if _SCHOOL_CITY in request.GET:
+        school.city     = request.GET[_SCHOOL_CITY]
 
-    if _DESCRIPTION in request.GET:
-        response[_DESCRIPTION] = str(request.GET[_DESCRIPTION])
+    if _SCHOOL_ZIP in request.GET:
+        school.zipCode  = request.GET[_SCHOOL_ZIP]
 
-    response = json.dumps(response)
+    if _SCHOOL_TYPE in request.GET:
+        school.type     = request.GET[_SCHOOL_TYPE]
 
-    return StreamingHttpResponse(response, content_type='application/response')
+    if _SCHOOL_WEB in request.GET:
+        school.webSite  = request.GET[_SCHOOL_WEB]
+
+    school.save()
+
+    return HttpResponse(status=_ADD_OR_MODIFY_RECORD_SUCCESS)
+
+def addModifyDepartment(request):
+
+    dept    = 0
+
+    if _DEPT_NAME in request.GET:
+        dept = Department.objects.get_or_create(name=_DEPT_NAME)
+    else:
+        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
+
+    if _DEPT_DESC in request.GET:
+        dept.description    = request.GET[_DEPT_DESC]
+
+    dept.save()
+
+    return HttpResponse(status=_ADD_OR_MODIFY_RECORD_SUCCESS)
+
+def addModifyCourse(request):
+
+    course = 0
+
+    if _COURSE_SIGLE in request.GET:
+        course = Course.objects.get_or_create(sigle=request.GET[_COURSE_SIGLE])
+    else:
+        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
+
+    if _COURSE_NAME in request.GET:
+        course.name = request.GET[_COURSE_NAME]
+
+    if _COURSE_DESC in request.GET:
+        course.description = request.GET[_COURSE_DESC]
+
+    course.save()
+
+    return HttpResponse(status=_ADD_OR_MODIFY_RECORD_SUCCESS)
 
 def addModififyBook(request):
 
     studentID       = ""
-    studentIntent   = ""
-    bookTitle       = ""
-    bookAuthor      = ""
-    bookEdition     = ""
     bookCourse      = ""
-    bookState       = ""
-    bookDesc        = ""
-    bookISBN        = ""
-    bookPrice       = ""
-    bookImage       = ""
 
-    if _USER in request.GET:
-        studentID = request.GET[_USER]
+    # Si l'usager n'est pas specifie, alors le service retourne une erreur
+    if _BOOK_USER in request.GET:
+        studentID = Account.objetcs.get_object_or_404(id=studentID)
+    else:
+        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
 
-    if _INTENT in request.GET:
-        studentIntent = request.GET[_INTENT]
+    # Si le sigle n'est pas specifie, alors le service retourne une erreur
+    if _BOOK_SIGLE in request.GET:
+        bookCourse = request.GET[_BOOK_SIGLE]
+    else:
+        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
 
-    if _TITLE in request.GET:
-        bookTitle = request.GET[_TITLE]
+    # Recherche ou creation du livre avec les donnees de base
+    book = Book.objects.get_or_create(owner=studentID, sigle=bookCourse)
 
-    if _AUTHOR in request.GET:
-        bookAuthor = request.GET[_AUTHOR]
+    if _BOOK_INTENT in request.GET:
+        book.intent = request.GET[_BOOK_INTENT]
 
-    if _EDITION in request.GET:
-        bookEdition = request.GET[_EDITION]
+    if _BOOK_TITLE in request.GET:
+        book.title = request.GET[_BOOK_TITLE]
 
-    if _SIGLE in request.GET:
-        bookCourse = request.GET[_SIGLE]
+    if _BOOK_AUTHOR in request.GET:
+        book.author = request.GET[_BOOK_AUTHOR]
 
-    if _STATE in request.GET:
-        bookState = request.GET[_STATE]
+    if _BOOK_EDITION in request.GET:
+        book.edition = request.GET[_BOOK_EDITION]
 
-    if _PRICE in request.GET:
-        bookPrice = request.GET[_PRICE]
+    if _BOOK_STATE in request.GET:
+        book.howIsBook = request.GET[_BOOK_STATE]
 
-    if _DESCRIPTION in request.GET:
-        bookDesc = request.GET[_DESCRIPTION]
+    if _BOOK_PRICE in request.GET:
+        book.price = request.GET[_BOOK_PRICE]
 
-    if _ISBN in request.GET:
-        bookISBN = request.GET[_ISBN]
+    if _BOOK_DESC in request.GET:
+        book.description = request.GET[_BOOK_DESC]
 
-    book = Book(owner=studentID,
-                sigle=bookCourse,
-                title=bookTitle,
-                author=bookAuthor,
-                edition=bookEdition,
-                price=bookPrice,
-                howIsBook=bookState,
-                description=bookDesc,
-                bookISBN=bookISBN)
+    if _BOOK_ISBN in request.GET:
+        book.ISBN = request.GET[_BOOK_ISBN]
 
+    # Sauvegarde du livre
     book.save()
 
-    return HttpResponse("SUCCESS")
+    return HttpResponse(status=_ADD_OR_MODIFY_RECORD_SUCCESS)
+
+def findBook(request):
+
+    response = {}
+
+    if _BOOK_TITLE in request.GET:
+        response[_BOOK_TITLE] = str(request.GET[_BOOK_TITLE])
+
+    if _BOOK_AUTHOR in request.GET:
+        response[_BOOK_AUTHOR] = str(request.GET[_BOOK_AUTHOR])
+
+    if _BOOK_EDITION in request.GET:
+        response[_BOOK_EDITION] = str(request.GET[_BOOK_EDITION])
+
+    if _BOOK_SIGLE in request.GET:
+        response[_BOOK_SIGLE] = str(request.GET[_BOOK_SIGLE])
+
+    if _BOOK_STATE in request.GET:
+        response[_BOOK_STATE] = str(request.GET[_BOOK_STATE])
+
+    if _BOOK_DESC in request.GET:
+        response[_BOOK_DESC] = str(request.GET[_BOOK_DESC])
+
+    response = json.dumps(response)
+
+    return StreamingHttpResponse(response, content_type='application/response')
