6864a8f51d0c654e8d085c7806f1161598a3fae7
Julien Drolet-Noel <eurojulien@gmail.com>
2014-07-01 17:12:10 -0400

    Modifications pour etre compatible avec HEROKU

--- LETSWEB/wsgi.py
+++ LETSWEB/wsgi.py
@@ -11,4 +11,10 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LETSWEB.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+# HEROKU
+from dj_static import Cling
+
+# HEROKU
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
