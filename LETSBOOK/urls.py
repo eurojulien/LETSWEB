95da2eb296f0a2c8644d9caba9dc67f81f58cd53
Julien Drolet-Noel <eurojulien@gmail.com>
2014-07-05 14:17:16 -0400

    url
    Mise a jour des services desservis

--- LETSBOOK/urls.py
+++ LETSBOOK/urls.py
@@ -5,8 +5,7 @@ from LETSBOOK import views
 urlpatterns = patterns('',
 
     url(r'school',       views.school,      name='school'),
-    url(r'department',   views.addModifyDepartment,  name='addModifyDepartment'),
-    url(r'course',       views.addModifyCourse,      name='addModifyCourse'),
     url(r'book',         views.book,        name='book'),
+    url(r'user',         views.user,        name='user'),
 
 )
