b672d3cc8ac792c6484a2ce2ec5310abcb8985f3
Julien Drolet-Noel <eurojulien@gmail.com>
2014-07-22 14:07:32 -0400

    Url
    Ajout du service course

--- LETSBOOK/urls.py
+++ LETSBOOK/urls.py
@@ -5,6 +5,7 @@ from LETSBOOK import views
 urlpatterns = patterns('',
 
     url(r'school',       views.school,      name='school'),
+    url(r'course',       views.course,      name='course'),
     url(r'book',         views.book,        name='book'),
     url(r'user',         views.user,        name='user'),
 
