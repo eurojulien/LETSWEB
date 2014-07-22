d67afa62b21cbecce4a250d14cd238d0f25fd3c3
Julien Drolet-Noel <eurojulien@gmail.com>
2014-07-22 14:07:12 -0400

    SchoolHandler
    Récuperation des cours fonctionnelle

--- LETSBOOK/requesthandler/SchoolHandler.py
+++ LETSBOOK/requesthandler/SchoolHandler.py
@@ -1,100 +1,76 @@
+__author__ = 'julien'
+
+from LETSBOOK.models import Course
+from LETSBOOK.models import Department
 from LETSBOOK.models import Establishment
 from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 import json
+
+_COURSE_DEPT    = "iddept"
+_COURSE_SCHOOL  = "idschool"
+_DEPT_COURSE    = "idcourse"
+_DEPT_SCHOOL    = "iddept"
 
 # Code de serveur
 # http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-_ADD_OR_MODIFY_RECORD_SUCCESS   = 201
-_ADD_OR_MODIFY_RECORD_FAIL      = 202
+_HTTP_SUCCESS                   = 200
+_HTTP_ERROR                     = 500
+_HTTP_JSON                      = "application/json"
 
-# Description d'un etablissement scolaire
-_SCHOOL_ID      = "id"
-_SCHOOL_NAME    = "name"
-_SCHOOL_STREET  = "street"
-_SCHOOL_CITY    = "city"
-_SCHOOL_ZIP     = "zipcode"
-_SCHOOL_TYPE    = "type"
-_SCHOOL_WEB     = "website"
+def getCourse(request):
 
-# Ajout ou modification d'un etablissement scolaire
-def putSchool(request):
+    params  = Q()
+    courses = None
 
-    school = Establishment()
-
-    if _SCHOOL_NAME in request.GET:
+    # Tous les cours d'un departement
+    if _COURSE_DEPT in request.GET:
 
         try:
-            school = Establishment.objects.get(name=request.GET[_SCHOOL_NAME])
+            dept    = Department.objects.get(pk=request.GET[_COURSE_DEPT])
+        except Department.DoesNotExist:
+            return HttpResponse(status=_HTTP_ERROR, content="Le departement " + request.GET[_COURSE_DEPT] + " n'existe pas")
 
-        except ObjectDoesNotExist:
-            school = Establishment(name=request.GET[_SCHOOL_NAME])
+        results = Course.objects.filter(department=dept)
 
-    else:
-        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
+        courses = {"courses" : []}
 
-    if _SCHOOL_STREET in request.GET:
-        school.street   = request.GET[_SCHOOL_STREET]
+        for index in range(0, results.count()):
+            courses["courses"].append(getCourseInfo(results[index], False))
 
-    if _SCHOOL_CITY in request.GET:
-        school.city     = request.GET[_SCHOOL_CITY]
+        return HttpResponse(status=_HTTP_SUCCESS, content=json.dumps(courses), content_type=_HTTP_JSON)
 
-    if _SCHOOL_ZIP in request.GET:
-        school.zipCode  = request.GET[_SCHOOL_ZIP]
 
-    if _SCHOOL_TYPE in request.GET:
-        school.type     = request.GET[_SCHOOL_TYPE]
+    # Tous les cours
+    results = Course.objects.all()
 
-    if _SCHOOL_WEB in request.GET:
-        school.webSite  = request.GET[_SCHOOL_WEB]
-
-    school.save()
-
-    return HttpResponse(status=_ADD_OR_MODIFY_RECORD_SUCCESS)
-
-# Recherche d'un etablissement scolaire
-def getSchool(request):
-
-    schools = 0
-
-    if _SCHOOL_ID in request.GET:
-        schools = Establishment.objects.get(pk=request.GET[_SCHOOL_ID])
-        return HttpResponse(schools.getJson(), content_type='application/response')
-
-    params = {}
-
-    if _SCHOOL_NAME in request.GET:
-        params['name__icontains']     = str(request.GET[_SCHOOL_NAME])
-
-    if _SCHOOL_STREET in request.GET:
-        params['street__icontains']   = str(request.GET[_SCHOOL_STREET])
-
-    if _SCHOOL_TYPE in request.GET:
-        params['type__icontains']     = str(request.GET[_SCHOOL_TYPE])
-
-    if _SCHOOL_CITY in request.GET:
-        params['city__icontains']     = str(request.GET[_SCHOOL_CITY])
-
-    if _SCHOOL_ZIP in request.GET:
-        params['zipCode__icontains']  = str(request.GET[_SCHOOL_ZIP])
-
-    if _SCHOOL_WEB in request.GET:
-        params['webSite__icontains']  = str(request.GET[_SCHOOL_WEB])
-
-    response = {}
-    results = Establishment.objects.filter(**params)
+    courses = {"courses" : []}
 
     for index in range(0, results.count()):
-        response[index] = results[index].getStr()
+        courses["courses"].append(getCourseInfo(results[index], False))
 
-    return HttpResponse(json.dumps(response), content_type='application/response')
+    return HttpResponse(status=_HTTP_SUCCESS, content=json.dumps(courses), content_type=_HTTP_JSON)
 
-def deleteSchool(request):
+def getCourseInfo(course, jsonFormat):
 
-    if _SCHOOL_ID in request.GET:
-        school = Establishment.objects.get(pk=request.GET[_SCHOOL_ID])
-        school.delete()
-        return HttpResponse(status=_ADD_OR_MODIFY_RECORD_SUCCESS)
+    cours = { "course"  : {
+        "sigle"         : str(course.sigle.encode('utf8', 'replace')),
+        "name"          : str(course.name.encode('utf8', 'replace')),
+        "description"   : str(course.description.encode('utf8', 'replace')),
+        "deptname"      : str(course.department.name.encode('utf8', 'replace')),
+        "deptdesc"      : str(course.department.description.encode('utf8', 'replace')),
+        "estaname"      : str(course.department.etablishment.name.encode('utf8', 'replace'))
+    }}
 
-    return HttpResponse(status=_ADD_OR_MODIFY_RECORD_FAIL)
+    if jsonFormat :
+        return json.dumps(cours)
+
+    return cours
+
+
+
+#def getDepartement(request):
+
+#def getUniversity(request):
+
 
