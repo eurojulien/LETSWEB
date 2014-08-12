5dcef6777a793285da6771d927b75cde20ee8515
Julien Drolet-Noel <eurojulien@gmail.com>
2014-08-12 00:52:12 -0400

    url
    suppresion de dept et cours
    non utilisé, school est utilisé a la place

--- LETSBOOK/urls.py
+++ LETSBOOK/urls.py
@@ -5,8 +5,6 @@ from LETSBOOK import views
 urlpatterns = patterns('',
 
     url(r'school',       views.school,      name='school'),
-    url(r'course',       views.course,      name='course'),
-    url(r'dept',         views.dept,        name='dept'),
     url(r'book',         views.book,        name='book'),
     url(r'user',         views.user,        name='user'),
 
