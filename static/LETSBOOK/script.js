ecdd1d76d5bf08319912ba94eb8b79a3e23aa8bc
Julien Drolet-Noel <eurojulien@gmail.com>
2014-08-25 00:02:55 -0400

    script.js
    Ajout méthode pour reload de la page chaque seconde (1000 milliseconds)
    La méthode est mise en commentaire

--- static/LETSBOOK/script.js
+++ static/LETSBOOK/script.js
@@ -1,7 +1,10 @@
 $(document).ready(
-
     function() {
         $('#tabs').tabify();
+        setInterval(reloadDocument, 1000);
     }
-
 )
+
+function reloadDocument(){
+    //location.reload();
+}
