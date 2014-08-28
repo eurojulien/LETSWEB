7ae2cf83d56c29ce82bb81a25bb8cd891a437cd8
Julien Drolet-Noel <eurojulien@gmail.com>
2014-08-27 22:07:51 -0400

    admin.py
    Suppression de toutes les classes
    Ajout de BookDescription

--- LETSBOOK/admin.py
+++ LETSBOOK/admin.py
@@ -4,8 +4,4 @@ from django.contrib import admin
 from django.contrib import admin
 from LETSBOOK.models import *
 
-admin.site.register(Establishment)
-admin.site.register(Department)
-admin.site.register(Course)
-admin.site.register(Book)
-admin.site.register(Account)
+admin.site.register(BookDescription)
