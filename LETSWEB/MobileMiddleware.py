e07fbe567440d8a90be185d7ef08fc4b66f5973b
Julien Drolet-Noel <eurojulien@gmail.com>
2014-12-14 00:03:01 -0500

    FRESH START

--- LETSWEB/MobileMiddleware.py
+++ LETSWEB/MobileMiddleware.py
@@ -1,1 +1,13 @@
 __author__ = 'julien'
+from django.conf import settings
+
+# Choix des templates
+# Desktop Browser   = Desktop Template
+# Mobile Browser    = Mobile Template
+class MobileMiddleware(object):
+    def process_request(self, request):
+        subdomain = request.META.get('HTTP_HOST', '').split('.')
+        if 'm' in subdomain:
+            settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS
+        else:
+            settings.TEMPLATE_DIRS = settings.DESKTOP_TEMPLATE_DIRS
--- LETSWEB/settings.py
+++ LETSWEB/settings.py
@@ -57,14 +57,13 @@ WSGI_APPLICATION\
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'LetsbookBD'),
     }
 }
-'''
 
 # POSTGRESQL
 '''
